1b745d39e84d92d6e741f38296e1e283da7ce387
SMART24\User <fds44fs@nate.com>
2025-05-12 15:50:18 +0900

    no message

---
+++ lib/home_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('TO-Do 텝')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context, "/todo-local"),
+                child: const Text("로컬 TO-DO")
+            ),
+            ElevatedButton(
+                onPressed: () => Navigator.pushNamed(context, "/todo-remote"),
+                child: const Text("클라우드 TO-DO")
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
